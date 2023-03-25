7287c876901f89acce39ecf85601952c82d7b04c
anatmart <anatmartkamjan@gmail.com>
2023-03-26 03:31:28 +0700

    change title

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>jRange demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="css/jquery.range.css">
+	<style>
+	#price_range {
+		-ms-touch-action: none;
+		touch-action: none;
+	}
+	</style>
 </head>
 
 <body>
@@ -95,6 +101,7 @@
 
     <script>
         $(function() {
+
             $('#price_range').jRange({
                 from: 0,
                 to: 3000,
@@ -113,6 +120,7 @@
     <script>
         function filterProducts() {
             var price_range = $('#price_range').val();
+
             $.ajax({
                 type: 'POST',
                 url: 'fetchProducts.php',
